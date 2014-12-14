8e11d716bf27ce63849c7e3e2fa1ab0be5abfbbb
Dave Byrne <david.r.byrne@gmail.com>
2014-12-13 21:21:03 -0500

    added a second servo

--- DocOckClaws/DocOckClaws.ino
+++ DocOckClaws/DocOckClaws.ino
@@ -1,31 +1,34 @@
 #include <Servo.h> 
  
-Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
+Servo servo1;
+Servo servo2;
  
-int pos = 0;    // variable to store the servo position 
+int pos = 0;
  
 void setup() 
 { 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  servo1.attach(9);
+  servo2.attach(10);
 } 
  
  
 void loop() 
 { 
   int delayTime = random(3,40);
-  for(pos = 20; pos < 165; pos += 1)  // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(delayTime);                       // waits 15ms for the servo to reach the position 
+  for(pos = 20; pos < 165; pos += 1)  
+  {                                   
+    servo1.write(pos);   
+    servo2.write(pos);       
+    delay(delayTime);             
   } 
   delayTime = random(4000);
   delay(delayTime);
   delayTime = random(3,40);
-  for(pos = 165; pos>=20; pos-=1)     // goes from 180 degrees to 0 degrees 
+  for(pos = 165; pos>=20; pos-=1)  
   {                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(delayTime);                       // waits 15ms for the servo to reach the position 
+    servo1.write(pos);
+    servo2.write(pos);       
+    delay(delayTime);            
   }
   delayTime = random(4000);
   delay(delayTime); 
